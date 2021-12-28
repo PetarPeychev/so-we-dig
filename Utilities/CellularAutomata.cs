e9f9aefc08b7e1f5685f7238ba643c8382380cc0
PetarPeychev <petarpeychev98@gmail.com>
2021-12-28 21:14:59 +0000

    cellular automata

---
+++ Utilities/CellularAutomata.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoWeDig.Utilities
+{
+    class CellularAutomata
+    {
+        private float initialDensity;
+        private int steps;
+        private int lonelinessLimit;
+
+        public CellularAutomata(float initialDensity, int steps, int lonelinessLimit)
+        {
+            this.initialDensity = initialDensity;
+            this.steps = steps;
+            this.lonelinessLimit = lonelinessLimit;
+        }
+
+        public bool[,] GenerateTerrain(int width, int height, int seed)
+        {
+
+        }
+    }
+}
